38772c05feca47c81b82abd9b88a39ea8ee56e10
lol0320 <lol0320@naver.com>
2020-09-24 00:13:56 +0900

    column1

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 function onScroll() {
 	var sct = $(this).scrollTop();
 	if(sct > 0) {
-		$(".header-wrapper .top-wrap").css("background-color", "rgba(0,0,0,0.7)");
+		$(".header-wrapper .top-wrap").css("background-color", "rgba(0,0,0,0.8)");
 	}
 	else {
 		$(".header-wrapper .top-wrap").css("background-color", "rgba(0,0,0,0)");

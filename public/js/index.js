dc678b1db2004671f551397ddc970bb6917b70a0
lol0320 <lol0320@naver.com>
2020-09-25 17:26:27 +0900

    0925

--- public/js/index.js
+++ public/js/index.js
@@ -25,3 +25,56 @@ function onSlidLeave(){
 $(window).scroll(onScroll);
 $(".slider-txt1").mouseenter(onSlidEnter);
 $(".slider-txt1").mouseleave(onSlidLeave);
+
+
+(function(){
+	var $slideStage = $(".header-wrapper .slider-stage");
+	var $slideWrap = $(".header-wrapper .slider-wrapper1");
+	var $btnPrev = $(".slider-txt1 .slider-btn");
+	var $btnNext = $(".slider-txt1 .slider-btn");
+	var $pager;
+	var $slides = [];
+	var idx = 0;
+	var lastIdx = slides.length - 1;
+	var interval;
+
+	
+
+	function slideInit() {
+		$slideWrap.html($slides[idx].clone());
+	}
+
+	$btnPrev.click(onPrev);
+	$btnNext.click(onNext);
+	$slideStage.mouseover(onHover);
+	$slideStage.mouseleave(onLeave);
+
+	function onHover() {
+		clearInterval(interval);
+	}
+
+	function onLeave() {
+		interval = setInterval(onNext, 3000);
+	}
+
+	function onPrev() {
+		idx = (idx == 0) ? lastIdx : idx - 1;
+		ani();
+	}
+
+	function onNext() {
+		idx = (idx == lastIdx) ? 0 : idx + 1;
+		ani();
+	}
+
+	function onPagerClick() {
+		idx = $(this).index();
+		ani();
+	}
+
+	function ani() {
+		$pager.removeClass("active").eq(idx).addClass("active");
+		$($slides[idx].clone()).appendTo($slideWrap).stop().animate({"opacity": 1}, 500, slideInit);
+	}
+	
+});

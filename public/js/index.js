4b52611727ad1444096442c6c0e61c6ccd9b5052
lol0320 <lol0320@naver.com>
2020-09-25 18:20:23 +0900

    09-25

--- public/js/index.js
+++ public/js/index.js
@@ -37,54 +37,13 @@ $(".slider-txt1").mouseenter(onSlidEnter);
 $(".slider-txt1").mouseleave(onSlidLeave);
 
 
-(function () {
-	var $slideStage = $(".header-wrapper .slider-stage");
-	var $slideWrap = $(".header-wrapper .slider-wrapper1");
-	var $btnPrev = $(".slider-txt1 .slider-btn");
-	var $btnNext = $(".slider-txt1 .slider-btn");
-	var $pager;
-	var $slides = $slideStage.children("div");
-	console.log($slides);
-	var idx = 0;
-	var lastIdx = $slides.length - 1;
-	var interval;
 
 
-	function slideInit() {
-		$slideWrap.html($slides[idx].clone());
-	}
 
-	$btnPrev.click(onPrev);
-	$btnNext.click(onNext);
-	$slideStage.mouseover(onHover);
-	$slideStage.mouseleave(onLeave);
+$(".slider-btn .btn").click(function(){
+	var idx=0;
+	idx = idx == 1 ? idx = 0 : idx = 1;
+	$(".slider-wrapper").removeClass('on');
+	$(".slider-wrapper").eq(idx).addClass();
 
-	function onHover() {
-		clearInterval(interval);
-	}
-
-	function onLeave() {
-		interval = setInterval(onNext, 3000);
-	}
-
-	function onPrev() {
-		idx = (idx == 0) ? lastIdx : idx - 1;
-		ani();
-	}
-
-	function onNext() {
-		idx = (idx == lastIdx) ? 0 : idx + 1;
-		ani();
-	}
-
-	function onPagerClick() {
-		idx = $(this).index();
-		ani();
-	}
-
-	function ani() {
-		$pager.removeClass("active").eq(idx).addClass("active");
-		$($slides[idx].clone()).appendTo($slideWrap).stop().animate({ "opacity": 1 }, 500, slideInit);
-	}
-
-})();
+});
